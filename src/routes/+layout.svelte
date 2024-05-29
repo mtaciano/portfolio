c9eaf87073abd5da38bd0d4ff0d6b0c3efb721d2
Miguel Taciano <contact@mtaciano.com>
2024-05-28 22:23:22 -0300

    Add font-display and insights

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -2,6 +2,9 @@
   import "../app.scss";
   import Footer from "$lib/components/Footer.svelte";
   import Header from "$lib/components/Header.svelte";
+  import { injectSpeedInsights } from "@vercel/speed-insights/sveltekit";
+
+  injectSpeedInsights();
 </script>
 
 <Header />
