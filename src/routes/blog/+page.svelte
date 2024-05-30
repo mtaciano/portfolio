9e4d41e7be3829439eb9d90928d4c749604044f0
Miguel Taciano <contact@mtaciano.com>
2024-05-30 18:52:55 -0300

    Add blog page
    I don't plan on updating it so soon, since I have other priorities at
    the moment. Therefore, I'll put a placeholder text to warn users that
    the blog is not yet implemented, and will not be so soon.

--- src/routes/blog/+page.svelte
+++ src/routes/blog/+page.svelte
@@ -2,15 +2,46 @@
   <title>Miguel Taciano :: Blog</title>
 </svelte:head>
 
-<h1>Blog</h1>
+<div class="outer">
+  <h1>Coming Soon<sup>TM</sup></h1>
+</div>
 
 <style lang="scss">
-  h1 {
-    font-family: var(--font-serif);
-    font-weight: 900;
-    font-size: 6rem;
-
+  * {
     text-align: center;
-    text-decoration: underline;
+  }
+
+  .outer {
+    margin: {
+      top: 5rem;
+      bottom: 5rem;
+    }
+  }
+
+  h1 {
+    color: var(--text-color-strong);
+    font: {
+      family: var(--font-serif);
+      size: clamp(4.125rem, 10vw, 6.125rem);
+      weight: 900;
+    }
+    text-decoration: {
+      line: underline;
+      style: solid;
+      color: var(--theme-color-normal);
+      thickness: 0.175em;
+    }
+    padding: {
+      left: 1rem;
+      right: 1rem;
+    }
+  }
+
+  sup {
+    font: {
+      family: inherit;
+      size: 0.5em;
+      weight: inherit;
+    }
   }
 </style>
